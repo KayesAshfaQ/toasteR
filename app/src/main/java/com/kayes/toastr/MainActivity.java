31c9c5f5f0ee3e23ba171955a216c1577a9b9dbc
KayesAshfaQ <asfak7074@gmail.com>
2021-11-19 03:48:49 +0600

    minor bug fixed

--- app/src/main/java/com/kayes/toastr/MainActivity.java
+++ app/src/main/java/com/kayes/toastr/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-
+import com.kayes.custom_toaster.ToasteR;
 
 
 public class MainActivity extends AppCompatActivity {
